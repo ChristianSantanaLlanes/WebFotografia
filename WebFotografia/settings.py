3a92d63fe26e44886aac96f8ace458ab8eff5443
ChristianSantanaLlanes <cs632706@gmail.com>
2022-02-11 21:00:32 -0500

    Creo que ahora funciona

--- WebFotografia/settings.py
+++ WebFotografia/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR / 'media'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 
 
@@ -176,3 +176,4 @@ LOGGING = {
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+
